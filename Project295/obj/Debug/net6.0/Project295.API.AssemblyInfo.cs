2bcf110ca4316b8694e2cb5444e0d426e2033af6
Ahmad-ALSmadi <smadiahmed88@gmail.com>
2025-01-18 12:30:49 +0300

    Edit

--- Project295/obj/Debug/net6.0/Project295.API.AssemblyInfo.cs
+++ Project295/obj/Debug/net6.0/Project295.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project295.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38f9235aa5a6e632976836260d6b31175084e532")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ea1c1f0547549ebc8add8bc28ff8ba17d95665")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project295.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project295.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
